d5bac71385e70227bc064d5d36091406e643d798
little-meow-meow <204685920+little-meow-meow@users.noreply.github.com>
2025-09-02 04:21:11 -0700

    checkpoint
    Signed-off-by: little-meow-meow <204685920+little-meow-meow@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import logging
 import os
 import sys
+import time
+import traceback
 
 import discord
 import dotenv
+from discord import DiscordException
 
 from morticia import Morticia
-from utils import get_pr_links_from_text
+from utils import get_pr_links_from_text, pretty_duration
 
 dotenv.load_dotenv(".env")
 
@@ -22,8 +25,29 @@ username = os.environ.get("GITHUB_BOT_USERNAME")
 email = os.environ.get("GITHUB_BOT_EMAIL")
 morticia = Morticia(token)
 
-intents = discord.Intents.default()
+intents = discord.Intents.all()
 bot = discord.Bot()
+
+
+def complains(func):
+    """
+    Decorator function for bot commands to automatically respond with any unhandled exceptions.
+    :param func:
+    :return:
+    """
+    async def wrapper(*args, **kwds):
+        try:
+            await func(*args, **kwds)
+        except Exception:
+            ctx: discord.ApplicationContext = args[0]
+            message = f"{ctx.user.mention} Unhandled exception:\n```\n{traceback.format_exc()}```"
+            try:
+                await ctx.respond(message)
+            except DiscordException:
+                await ctx.channel.send(message)
+        return None
+
+    return wrapper
 
 
 @bot.event
@@ -32,13 +56,15 @@ async def on_ready():
 
 
 @bot.message_command(
+    name="explore",
     description="Open a dialogue of actions for a given PR.",
     default_member_permissions=discord.Permissions(
         discord.Permissions.ban_members.flag
     ),
     guild_ids=[os.environ.get("DISCORD_GUILD_ID")],
 )
-async def port(ctx: discord.ApplicationContext, message: discord.Message):
+@complains
+async def explore(ctx: discord.ApplicationContext, message: discord.Message):
     matches: list[str] = get_pr_links_from_text(message.content)
     if matches == 0:
         await ctx.respond("Hey, I didn't find any pull request links there.")
@@ -46,7 +72,16 @@ async def port(ctx: discord.ApplicationContext, message: discord.Message):
 
     pull_request_url = matches[0]
 
-    await ctx.respond(f"This command is currently disabled.")
+    (organization_name, repo_name) = Morticia.repo_id_from_url(pull_request_url)
+    repo_id = f"{organization_name}/{repo_name}"
+    await ctx.respond(f"```\nFetching the latest changes to {repo_id} ...```")
+
+    start_time = time.time()
+    await morticia.pull_repo(pull_request_url)
+    end_time = time.time()
+    pretty_time = pretty_duration(int(end_time - start_time))
+
+    await ctx.respond(f"Done in {pretty_time}!")
 
 
 bot.run(os.environ.get("DISCORD_TOKEN"))
--- morticia.py
+++ morticia.py
@@ -1,8 +1,13 @@
 import logging
+import os
 
+from dulwich import porcelain
+from dulwich.errors import NotGitRepository
 from github import Github, Auth
+from slugify import slugify
 
 GITHUB_URL = "https://github.com/"
+REPOSITORIES_DIR = "./repositories"
 
 log = logging.getLogger(__name__)
 
@@ -11,6 +16,9 @@ class Morticia:
     def __init__(self, auth_token: str):
         self.auth = Auth.Token(auth_token)
         self.github = Github(auth=self.auth)
+
+    def close(self) -> None:
+        self.github.close()
 
     @classmethod
     def repo_id_from_url(cls, url: str) -> tuple[str, str]:
@@ -22,14 +30,49 @@ class Morticia:
         )
 
     @classmethod
+    def repo_base_url(cls, url: str) -> str:
+        """
+        Takes a GitHub URL relevant to a repository and returns the URL to that repository.
+        :param url:
+        :return:
+        """
+        (organization_name, repo_name) = Morticia.repo_id_from_url(url)
+        return f"{GITHUB_URL}{organization_name}/{repo_name}"
+
+    @classmethod
+    def repo_slug_from_url(cls, url: str) -> str:
+        """
+        Takes a GitHub URL relevant to a repository and returns an org-repo slug for that repository.
+        :param url:
+        :return:
+        """
+        (organization_name, repo_name) = Morticia.repo_id_from_url(url)
+        return slugify(f"{organization_name}/{repo_name}")
+
+    @classmethod
     def issue_id_from_url(cls, url: str) -> int:
         last_slash = url.rindex("/")
         return int(url[last_slash + 1:])
+
+    @classmethod
+    async def pull_repo(cls, url: str):
+        """
+        Pulls (or clones) a repository's default branch down to the ``./repositories`` directory.
+        """
+        os.makedirs(REPOSITORIES_DIR, exist_ok=True)
+        repo_url = Morticia.repo_base_url(url)
+        repo_slug = Morticia.repo_slug_from_url(repo_url)
+        repo_dir = f"{REPOSITORIES_DIR}/{repo_slug}"
+
+        try:
+            repo = porcelain.Repo(repo_dir)
+            porcelain.pull(repo, force=True)
+        except NotGitRepository as _:
+            repo = porcelain.clone(repo_url, repo_dir)
+
+        return repo
 
     def get_pull_request(self, url: str):
         repo = self.github.get_repo("/".join(Morticia.repo_id_from_url(url)))
         pr_id = Morticia.issue_id_from_url(url)
         return repo.get_pull(pr_id)
-
-    def close(self) -> None:
-        self.github.close()
