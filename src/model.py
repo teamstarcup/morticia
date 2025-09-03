fef7c02c85e227d89f90d0f9d92cfcbde9c942e8
little-meow-meow <204685920+little-meow-meow@users.noreply.github.com>
2025-09-03 04:40:10 -0700

    add known_pull_requests and known_file_changes tables
    Signed-off-by: little-meow-meow <204685920+little-meow-meow@users.noreply.github.com>

--- src/model.py
+++ src/model.py
@@ -1,6 +1,18 @@
-from sqlalchemy import String, MetaData, ForeignKey
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from enum import Enum
+from typing import Optional
 
+from sqlalchemy import MetaData, ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+class FileChangeStatus(Enum):
+    ADDED = "added"
+    REMOVED = "removed"
+    MODIFIED = "modified"
+    RENAMED = "renamed"
+
+    COPIED = "copied"
+    CHANGED = "changed"
+    UNCHANGED = "unchanged"
 
 class Base(DeclarativeBase):
     metadata = MetaData(naming_convention={
@@ -22,3 +34,47 @@ class KnownFile(Base):
 
     repo_id: Mapped[KnownRepo] = mapped_column(ForeignKey("known_repos.repo_id"), primary_key=True)
     file_path: Mapped[str] = mapped_column(primary_key=True)
+
+class KnownPullRequest(Base):
+    __tablename__ = "known_pull_requests"
+
+    pull_request_id: Mapped[int] = mapped_column(primary_key=True)
+    repo_id: Mapped[str] = mapped_column(ForeignKey("known_repos.repo_id"), primary_key=True)
+
+    title: Mapped[str]
+    body: Mapped[str]
+
+    state: Mapped[str]
+    merged: Mapped[bool]
+
+    additions: Mapped[int]
+    deletions: Mapped[int]
+
+    changed_files: Mapped[int]
+    commits: Mapped[int]
+    comments: Mapped[int]
+
+    ref_base: Mapped[str]
+    ref_head: Mapped[str]
+
+    created_at: Mapped[int]
+    updated_at: Mapped[Optional[int]]
+    closed_at: Mapped[Optional[int]]
+    merged_at: Mapped[Optional[int]]
+
+class KnownFileChange(Base):
+    __tablename__ = "known_file_changes"
+
+    pull_request_id: Mapped[str] = mapped_column(ForeignKey("known_pull_requests.pull_request_id"), primary_key=True)
+    repo_id: Mapped[str] = mapped_column(ForeignKey("known_repos.repo_id"), primary_key=True)
+    file_path: Mapped[str] = mapped_column(ForeignKey("known_files.file_path"), primary_key=True)
+
+    status: Mapped[str]
+
+    additions: Mapped[int]
+    changes: Mapped[int]
+    deletions: Mapped[int]
+
+    previous_file_path: Mapped[Optional[str]]
+    patch: Mapped[str]
+    sha: Mapped[str]
